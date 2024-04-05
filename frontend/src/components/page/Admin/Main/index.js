7f29b7523b8fbd2c2f473e7eed2b4795c0a3f6c8
raedalfakhoury <alfakhouryraed@gmail.com>
2024-04-05 18:45:35 +0300

    get all barnch 30%

--- frontend/src/components/page/Admin/Main/index.js
+++ frontend/src/components/page/Admin/Main/index.js
@@ -1,25 +1,70 @@
-/* eslint-disable no-unused-vars */
-import * as React from "react";
+import React, { useState, useEffect } from "react";
 import { DataGrid } from "@mui/x-data-grid";
-import { useDemoData } from "@mui/x-data-grid-generator";
 import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import axios from "axios";
+
 export const Main = () => {
-  const { data } = useDemoData({
-    dataSet: "Commodity",
-    rowLength: 10,
-    maxColumns: 5,
-  });
+  const [allBranch, setAllBranch] = useState([]);
+
   const openPickerButtonStyle = {
     fontSize: "12px",
     width: "20px",
   };
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/restaurant/")
+      .then((result) => {
+        const rowsWithId = result.data.result.map((row) => ({
+          id: row.restaurant_id,
+          ...row,
+        }));
+        setAllBranch(rowsWithId);
+        console.log(rowsWithId);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const handleDelete = (id) => {
+    // Implement delete logic here
+    console.log("Deleting row with ID:", id);
+  };
+
+  const columns = [
+    { field: "restaurant_name", headerName: "Restaurant Name", width: 150 },
+    { field: "phone", headerName: "Phone", width: 150 },
+    { field: "street_name", headerName: "Street", width: 150 },
+    { field: "start_time", headerName: "Start Time", width: 200 },
+    { field: "end_time", headerName: "End Time", width: 200 },
+    { field: "nearby_landmarks", headerName: "Landmarks", width: 150 },
+    {
+      field: "actions",
+      headerName: "Delete",
+      width: 120,
+      renderCell: (params) => (
+        <IconButton
+          onClick={() => handleDelete(params.row.id)}
+          aria-label="delete"
+          style={{ width: "10px", color: "red" }}
+        >
+          <DeleteIcon />
+        </IconButton>
+      ),
+    },
+  ];
+
   return (
-    <div style={{ width: "100%", marginTop: "80px" }}>
+    <div style={{ width: "100%", marginTop: "80px", marginLeft: "2px" }}>
       <Box sx={{ mb: 1 }}></Box>
-      <div style={{ height: 400}}>
+      <div style={{ height: 600 }}>
         <DataGrid
           slotProps={{ baseIconButton: { style: openPickerButtonStyle } }}
-          {...data}
+          rows={allBranch}
+          columns={columns}
         />
       </div>
     </div>
